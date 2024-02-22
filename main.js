6035ea5ee02173fc404b87635fad3a81f63d6034
lakenspace2 <145363978+lakenspace2@users.noreply.github.com>
2024-02-22 11:50:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-const API_KEY = "AIzaSyAVPuNzgLBCXxWphoNQlRMjjErFDYyhA8o";
-var instructions = "instructions='Act like you are an AI for WMC (Western Michigan Christian).';backgroundinfo='Laken is a guy who coded and trained you. WMC is located in Muskegon, MI.';";
+const API_KEY = ("AIzaSyAVPuNzgLBCXxWphoNQlRMjjErFDYyhA8o");
+var instructions = ("instructions='Act like you are an AI for WMC (Western Michigan Christian).';backgroundinfo='Laken is a guy who coded and trained you. WMC is located in Muskegon, MI.';");
 var prevresponsetable = [" "," "," "];
 
 let TranslatedbellTimes = [28800,31920,32160,35040,35280,38160,38400,39780,40020,41520,41760,42900,43140,44640,44880,47760,48000,50880,51120,54000,115200];
-let timer = document.getElementById("timer")
-var interval = setInterval(functi
+let timer = document.getElementById("timer");
+var interval = setInterval(function() {
     let d = new Date
-    let s = d.getSeconds()
-    let m = d.getMinutes()
-    let h = d.getHours()
-    let closestTime = -1
+    let s = d.getSeconds();
+    let m = d.getMinutes();
+    let h = d.getHours();
+    let closestTime = -1;
     for (let i = 0; i<TranslatedbellTimes.length; i++) {
         if ((((h*3600)+(m*60)+s)>closestTime)&&(TranslatedbellTimes[i]>((h*3600)+(m*60)+s))) {
-            closestTime = TranslatedbellTimes[i]
+            closestTime = TranslatedbellTimes[i];
         }
     }
-    let difference = (closestTime-1)-((h*3600)+(m*60)+s)
-    let h_1 = difference/86400
-    let m_2 = difference/3600
-    let s_3 = difference/60
-    timer.innerText = (("0" + (Math.floor((h_1%1)*24))).slice(-2)+":"+("0" + Math.floor((m_2%1)*60)).slice(-2)+":"+("0" + Math.floor((s_3%1)*60)).slice(-2))
+    let difference = (closestTime-1)-((h*3600)+(m*60)+s);
+    let h_1 = difference/86400;
+    let m_2 = difference/3600;
+    let s_3 = difference/60;
+    var timeUntilNextBellWMC = (("0" + (Math.floor((h_1%1)*24))).slice(-2)+":"+("0" + Math.floor((m_2%1)*60)).slice(-2)+":"+("0" + Math.floor((s_3%1)*60)).slice(-2));
+    timer.innerText = timeUntilNextBellWMC;
 }, 500);
 
 function appendData(textfromai) {
-    prevresponsetable.unshift(textfromai)
+    prevresponsetable.unshift(textfromai);
     var scrollableSection = document.getElementById("responseContainer");
     var newDiv = document.createElement("div");
     newDiv.className = "response";
